9038bd6ffd2b2d30675c5cdd42ad0be388b4150e
syouzan420 <syouzanponkikki@yahoo.co.jp>
2025-12-11 19:56:43 +0900

    update

--- src/Activity.hs
+++ src/Activity.hs
@@ -2,38 +2,66 @@
 
 module Activity where
 
-import Control.Monad.Reader
-import Control.Monad.State
+import Control.Monad.Cont (Cont)
+import Control.Monad.Reader (MonadReader(ask))
+import Control.Monad.State (gets,modify,MonadTrans(lift))
+import Control.Monad.State.Class (MonadState)
 import qualified Data.Text as T
-import FRP.Yampa
-import Data.Point2
-import Data.Vector2
+import FRP.Yampa (Event(NoEvent,Event),SF,Time,constant,identity,iPre,after,edge,now,switch,returnA,(>>>),Arrow((&&&),first,arr))
+import Data.Point2 (Point2(Point2))
+import Data.Vector2 (vector2)
 
-import ControlsMaps
-import Lightarrow
-import Message
-import OfflineData
-import Output
-import Ppmn.Parameters
+import ControlsMaps (proseControl)
+import Lightarrow (dSwont,swont,timedInterp,timedSequence,Embedding(Embedding))
+import Message (Message(ProseScroll))
+import Output (drawRectangle,drawSpriteExplicit,playSoloSound,soundTrigger,Color(White,Translucent),DrawOrientation(Original))
+import Ppmn.Parameters (Ppmn(ppmnHitPoints,ppmnMaxHitPoints,ppmnCry),PpmnAction(actionUpdateObject,actionObject))
 import qualified TextBox as TB
-import SoundName
+import SoundName (SoundName(Accept))
+import OfflineData ((>.=),OfflineData,OfflineIO)
+import Controls (Controls)
+import SpriteName (SpriteName)
 
+stdCommentary :: (MonadTrans t,
+                  MonadReader
+                    (Embedding Controls OfflineIO a b)
+                    (t (Cont (SF a b))),
+                  MonadState s (t (Cont (SF a b)))) =>
+                   (s -> T.Text) -> t (Cont (SF a b)) OfflineIO
 stdCommentary = stdProse $ \text -> proseControl >>> TB.writer (TB.autoScrollingProse text)
 
+stdLecture :: (MonadTrans t,
+                  MonadReader
+                    (Embedding Controls OfflineIO a b)
+                    (t (Cont (SF a b))),
+                  MonadState s (t (Cont (SF a b)))) =>
+                   (s -> T.Text) -> t (Cont (SF a b)) OfflineIO
 stdLecture = stdProse (\text -> breathe `switch` const (speak text))
   where
     breathe = constant (TB.drawConstant "", NoEvent) &&& after 0.25 ()
     speak text = proseControl >>> TB.writer (TB.scrollingProse text)
 
+stdProse :: (MonadTrans t,
+             MonadReader
+               (Embedding Controls OfflineIO a b)
+               (t (Cont (SF a b))),
+             MonadState s (t (Cont (SF a b)))) =>
+            (T.Text -> SF Controls (OfflineIO,Event (OfflineData -> IO a2)))
+            -> (s -> T.Text) -> t (Cont (SF a b)) (OfflineData -> IO a2)
 stdProse textbox prose = do
     Embedding embed <- ask
     text <- gets prose
     let ls = T.lines text
-        first = init ls
+        first' = init ls
         final = last ls
-    sequence_ $ map ((>>= arrowWait) . lift . swont . embed . textbox) first
+    mapM_ ((>>= arrowWait) . lift . swont . embed . textbox) first'
     lift . swont $ embed (textbox final)
 
+fadeTo :: (MonadTrans t,
+           MonadReader
+            (Embedding a OfflineIO a1 b)
+            (t (Cont (SF a1 b)))) =>
+          Color -> Time -> t (Cont (SF a1 b)) ()
 fadeTo color duration = ask >>= \(Embedding embed) -> lift . swont $ embed fade
   where
     fade = timedSequence t (map mask colors ++ [final])
@@ -42,6 +70,11 @@ fadeTo color duration = ask >>= \(Embedding embed) -> lift . swont $ embed fade
     mask color = constant (drawRectangle 160 144 color (Point2 0 0), NoEvent)
     final = constant (error "fadeTo terminated", Event ())
 
+fadeFrom :: (MonadTrans t,
+             MonadReader
+              (Embedding a OfflineIO a1 b)
+              (t (Cont (SF a1 b)))) =>
+            Color -> Time -> t (Cont (SF a1 b)) ()
 fadeFrom color duration = ask >>= \(Embedding embed) -> lift . swont $ embed fade
   where
     fade = timedSequence t (map mask colors ++ [final])
@@ -50,41 +83,76 @@ fadeFrom color duration = ask >>= \(Embedding embed) -> lift . swont $ embed fad
     mask color = constant (drawRectangle 160 144 color (Point2 0 0), NoEvent)
     final = constant (error "fadeTo terminated", Event ())
 
+stdHesitation :: (MonadTrans t,
+                  MonadReader
+                    (Embedding a b2 a1 b1) (t (Cont (SF a1 b1)))
+                 ) =>
+                 b2 -> t (Cont (SF a1 b1)) ()
 stdHesitation output = ask >>= \(Embedding embed) -> lift . swont $ embed (hesitate 0.5 output)
 
+longHesitation :: (MonadTrans t,
+                   MonadReader
+                     (Embedding a b2 a1 b1) (t (Cont (SF a1 b1)))
+                  ) =>
+                  b2 -> t (Cont (SF a1 b1)) ()
 longHesitation output = ask >>= \(Embedding embed) -> lift . swont $ embed (hesitate 1.5 output)
 
+hesitate :: Time -> c -> SF b (c, Event ())
 hesitate t = (&&& after t ()) . constant
 
+stdWait :: (MonadTrans t,
+            MonadReader
+              (Embedding Controls b2 a1 b1) (t (Cont (SF a1 b1)))
+           ) =>
+           b2 -> t (Cont (SF a1 b1)) ()
 stdWait output = ask >>= \(Embedding embed) -> lift . swont $ embed (wait output)
 
+arrowWait :: (MonadTrans t,
+              MonadReader
+                (Embedding Controls OfflineIO a1 b1) (t (Cont (SF a1 b1)))
+             ) =>
+            (OfflineData -> IO a2) -> t (Cont (SF a1 b1)) ()
 arrowWait output = ask >>= \(Embedding embed) -> lift . swont $ embed $ (wait output >>> first draw) >>> arrowSound
   where
-    draw = (identity &&& (constant 0.5 >>> TB.waitArrow)) >>> (arr $ uncurry (>.=))
+    draw = (identity &&& (constant 0.5 >>> TB.waitArrow)) >>> arr (uncurry (>.=))
 
-wait output = constant output &&& (proseControl >>> arr (any isProseScroll) >>> edge)
+wait :: c -> SF Controls (c,Event ())
+wait output = constant output &&& (proseControl 
+                            >>> arr (any isProseScroll) >>> edge)
 
+arrowSound :: SF (OfflineData -> IO a2, Event a) (OfflineIO, Event a)
 arrowSound = proc (output, trigger) -> do
     sound    <- soundTrigger Accept -< trigger
     trigger' <- iPre NoEvent        -< trigger
     returnA -< (output >.= sound, trigger')
 
+isProseScroll :: Message -> Bool
 isProseScroll ProseScroll = True
 isProseScroll _           = False
 
+slide :: Point2 Time -> Point2 Time -> Time -> SpriteName -> SF t OfflineIO
 slide = slideStretch 1 1
 
+slideStretch :: Time -> Time -> Point2 Time -> Point2 Time -> Time
+                     -> SpriteName -> SF t OfflineIO
 slideStretch f0 f1 (Point2 x0 y0) (Point2 x1 y1) t1 sprite = proc _ -> do
     f    <- timedInterp f0 f1 t1 -< ()
     x    <- timedInterp x0 x1 t1 -< ()
     y    <- timedInterp y0 y1 t1 -< ()
     returnA -< drawSpriteExplicit Original f White (vector2 0 0) sprite (Point2 x y)
 
+slide' :: Point2 Time -> Point2 Time -> Time -> SF t (Point2 Time)
 slide' (Point2 x0 y0) (Point2 x1 y1) t1 = proc _ -> do
     x    <- timedInterp x0 x1 t1 -< ()
     y    <- timedInterp y0 y1 t1 -< ()
     returnA -< Point2 x y
 
+stdHPEffect :: (MonadTrans t,
+                MonadReader
+                  (Embedding a Time a1 b) (t (Cont (SF a1 b))),
+                MonadState s (t (Cont (SF a1 b))),
+                PpmnAction s) =>
+               Double -> t (Cont (SF a1 b)) Double
 stdHPEffect delta = do
     object <- gets actionObject
     Embedding embed <- ask
@@ -95,17 +163,33 @@ stdHPEffect delta = do
     modify (actionUpdateObject (object { ppmnHitPoints = final }))
     return final
 
+changeHp :: Double -> Double -> Double -> SF b (Time, Event Double)
 changeHp oldHP newHP maxHP = changer &&& after duration newHP
   where
-    duration = 1.5 * (abs $ newHP - oldHP) / maxHP
+    duration = 1.5 * abs (newHP - oldHP) / maxHP
     changer = timedInterp oldHP newHP duration
 
+playCry :: (MonadTrans t,
+            MonadReader
+              (Embedding a OfflineIO a1 b1) (t (Cont (SF a1 b1)))
+           ) =>
+           Ppmn -> t (Cont (SF a1 b1)) ()
 playCry p = momentary (playSoloSound (ppmnCry p))
 
+momentary :: (MonadTrans t,
+              MonadReader
+                (Embedding a b2 a1 b1) (t (Cont (SF a1 b1)))
+             ) =>
+             b2 -> t (Cont (SF a1 b1)) ()
 momentary output = do
     Embedding embed <- ask
     lift . dSwont $ embed $ constant output &&& now ()
 
+over :: (MonadTrans t,
+         MonadReader
+           (Embedding a b2 a1 b1) (t (Cont (SF a1 b1)))
+        ) =>
+        Time -> SF a b2 -> t (Cont (SF a1 b1)) ()
 over interval sf = do
     Embedding embed <- ask
     lift . swont $ embed $ sf &&& (after interval () >>> iPre NoEvent)
--- src/Intro.hs
+++ src/Intro.hs
@@ -2,59 +2,77 @@
 
 module Intro (intro) where
 
-import Control.Monad.RWS
-import Control.Monad.Reader
+import Control.Monad.Cont (Cont)
+import Control.Monad.State.Class (MonadState)
+import Control.Monad.RWS (RWST(runRWST),MonadReader(local),MonadTrans(lift))
 import qualified Data.Text as T
-import FRP.Yampa
-import Data.Point2
-import Data.Vector2
-import Data.AffineSpace
+import FRP.Yampa (constant,time,(<<<),(>>>),Arrow(arr),SF)
+import FRP.Yampa.Event (Event)
+import Data.Point2 (Point2(Point2))
+import Data.Vector2 (vector2)
+import Data.AffineSpace (AffineSpace((.+^),origin))
 
-import Activity
-import Controls
-import ControlsMaps
-import Field
-import LabelName
-import Lightarrow
-import Menu
-import MusicName
-import OfflineData
-import Output
-import ProseName
-import SoundName
-import SpriteName
-import StateClass
+import Activity (arrowWait,fadeFrom,fadeTo,momentary,over,slide,slideStretch,stdLecture,stdWait)
+import Controls (Controls)
+import ControlsMaps (menuControl)
+import Field (screenCenterOffset)
+import LabelName (LabelName(Steve,Jeff,Larry,Mark,Sergey))
+import Lightarrow (embedArr,Embedding(Embedding),Swont)
+import Menu (backgroundMenu,choiceOverlay,columnMenu)
+import MusicName (MusicName(CenterTheme,TitleTheme))
+import OfflineData ((>.=),OfflineData,OfflineIO)
+import Output (clearScreen,drawSprite,drawText,fadeOutMusic,playSoloSound,playSound,restartRepeatingMusic,sentence,Color(Black,White))
+import ProseName (ProseName (ButPinchYourselfBitchYoure, IWokeUpAndIts,WhereAmI,AmIDreaming,WelcomeToTheWorldOf,MyNameIsWokeI,InThisWorldThereAre,AndThenThereArePeople,PeopleUsePeopleForFights,LookAtYouYouLook,IKnowIveHeardThat,DoYouRememberYourName))
+import SoundName (SoundName(Ignoloof,Drop,Crash,Accept))
+import SpriteName (SpriteName(AvatarFront,Woke,Logo,IgnoloofFront,AvatarPortrait))
+import StateClass (TextSource(prose,label))
+import Message (Message)
 
 intro :: OfflineData -> Swont Controls OfflineIO T.Text 
-intro od = fmap (\(x, _, ()) -> x) $ runRWST k (Embedding id) od
+intro od = (\(x, _, ()) -> x) <$> runRWST k (Embedding id) od
   where
     k = do
         titleDrop
         titleShake
         titleScreen
         instructionScreen
-        local (const (embedArr (clearScreen Black >.=))) $ wakeup
-        local (const (embedArr (drawSprite Woke (Point2 54 34) >.=))) $ welcome
+        local (const (embedArr (clearScreen Black >.=))) wakeup
+        local (const (embedArr (drawSprite Woke (Point2 54 34) >.=))) welcome
         explanation
-        narration <- local (const (embedArr (drawSprite AvatarPortrait (Point2 54 34) >.=))) $ lookAtYou
+        narration <- local (const (embedArr (drawSprite AvatarPortrait (Point2 54 34) >.=))) lookAtYou
         over 0.25 $ arr (>.= narration) <<< slide (Point2 54 34) (Point2 80 34) 0.125 AvatarPortrait
-        name <- local (const (embedArr ((narration >.= drawSprite AvatarPortrait (Point2 80 34)) >.=))) $ pickName
+        name <- local (const (embedArr ((narration >.= drawSprite AvatarPortrait (Point2 80 34)) >.=))) pickName
         over 0.25 $ arr (>.= narration) <<< slide (Point2 80 34) (Point2 54 34) 0.125 AvatarPortrait
         narration <- local (const (embedArr (drawSprite AvatarPortrait (Point2 54 34) >.=))) $ letsGo (label name od)
         local (const (Embedding id)) $ enter narration
         lift $ return (label name od)
 
+titleDrop :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+                  ()
+                  OfflineData
+                  (Cont (SF Controls OfflineIO))
+                  ()
 titleDrop = do  momentary $ playSound Drop
-                over 1.0 $ time >>> arr drop >>> arr draw
-    where  drop t  = Point2 20 (98 * t * t - 66)
+                over 1.0 $ time >>> arr drp >>> arr draw
+    where  drp t  = Point2 20 (98 * t * t - 66)
            draw    = drawSprite Logo --drawText "PEOPLEMON"
 
+titleShake :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+                   ()
+                   OfflineData
+                   (Cont (SF Controls OfflineIO))
+                   ()
 titleShake = do  momentary $ draw (shake 0) >.= play
                  over 0.25 $ time >>> arr shake >>> arr draw
     where  shake t  = Point2 20 (32 + 3 * 1 / (1 + 8 * t) * sin (32 * pi * t))
            draw     = drawSprite Logo --drawText "PEOPLEMON"
            play     = playSound Crash >.= restartRepeatingMusic TitleTheme
 
+titleScreen :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+                    ()
+                    OfflineData
+                    (Cont (SF Controls OfflineIO))
+                    ()
 titleScreen = do  stdWait initialDraw 
                   momentary $ initialDraw >.= playSound SoundName.Accept
                   over 0.5 $ time >>> arr raise >>> arr drawTitle
@@ -65,6 +83,11 @@ titleScreen = do  stdWait initialDraw
                         drawText "Alex Stuart" (Point2 40 96)      >.=
                         drawText "PRESS Z TO BEGIN" (Point2 16 124)
 
+instructionScreen :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+                          ()
+                          OfflineData
+                          (Cont (SF Controls OfflineIO))
+                          ()
 instructionScreen = do  stdWait out
                         momentary $ out >.= playSound SoundName.Accept
                         local (const (embedArr (out >.=))) $ fadeTo Black 1.0
@@ -78,6 +101,11 @@ instructionScreen = do  stdWait out
                drawText "X:       Cancel" (Point2 8 100)   >.=
                drawText "PRESS Z (AGAIN!)" (Point2 16 124)
 
+wakeup :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+               ()
+               OfflineData
+               (Cont (SF Controls OfflineIO))
+               ()
 wakeup = do
     n1 <- stdLecture (prose IWokeUpAndIts)
     arrowWait n1
@@ -86,6 +114,11 @@ wakeup = do
     n3 <- stdLecture (prose AmIDreaming)
     arrowWait n3
 
+welcome :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+                ()
+                OfflineData
+                (Cont (SF Controls OfflineIO))
+                ()
 welcome = do
     fadeFrom Black 0.5
     momentary $ restartRepeatingMusic CenterTheme
@@ -95,6 +128,11 @@ welcome = do
     arrowWait n2
     fadeTo White 0.5 
 
+explanation :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+               ()
+               OfflineData
+               (Cont (SF Controls OfflineIO))
+               ()
 explanation = do
     n1 <- stdLecture (prose InThisWorldThereAre)
     arrowWait n1
@@ -106,7 +144,12 @@ explanation = do
         n3 <- stdLecture (prose PeopleUsePeopleForFights)
         arrowWait n3
         fadeTo White 0.5
-
+ 
+lookAtYou :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+             ()
+             OfflineData
+             (Cont (SF Controls OfflineIO))
+             OfflineIO 
 lookAtYou = do
     fadeFrom White 0.5
     n1 <- stdLecture (prose LookAtYouYouLook)
@@ -115,22 +158,41 @@ lookAtYou = do
     arrowWait n2
     return n2
 
+pickName :: RWST (Embedding Controls OfflineIO Controls OfflineIO)
+            ()
+            OfflineData
+            (Cont (SF Controls OfflineIO))
+            LabelName
 pickName = do
     (next, k) <- choiceOverlay (menuControl >>> nameMenu (const pickName))
     next
 
+nameMenu :: Monad m => (() -> m LabelName)
+                    -> SF [Message] (OfflineIO,Event(m LabelName,Int))
 nameMenu cancel = backgroundMenu 80 96 (Point2 0 0) menu
   where
     menu = columnMenu (map (\n -> (n, return n)) names) (cancel (), 0) 0
 
+names :: [LabelName]
 names = [ Jeff, Larry, Mark, Sergey, Steve ]
 
+letsGo :: (MonadTrans t,
+           MonadReader
+            (Embedding Controls OfflineIO a b)
+            (t (Cont (SF a b))),
+           MonadState s (t (Cont (SF a b))),
+           TextSource s) =>
+             T.Text -> t (Cont (SF a b)) OfflineIO
 letsGo name = do
     n1 <- stdLecture (\s -> sentence '!' ["Good,", name] `T.append` " " `T.append` prose IKnowIveHeardThat s)
     arrowWait n1
-    n2 <- stdLecture (prose ButPinchYourselfBitchYoure)
-    return n2
+    stdLecture (prose ButPinchYourselfBitchYoure)
 
+enter :: (MonadTrans t,
+          MonadReader
+            (Embedding a OfflineIO a1 b1)
+            (t (Cont (SF a1 b1)))) =>
+          OfflineIO -> t (Cont (SF a1 b1)) ()
 enter n2 = do
     over 1.0 $ arr (>.= n2) <<< slideStretch 1 0.45 (Point2 54 34) (Point2 60 52) 1.0 AvatarPortrait
     over 0.5 $ constant (drawSprite AvatarFront ((origin .+^ screenCenterOffset) .+^ (vector2 0 (-8))) >.= n2)
